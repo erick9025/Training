49cafc785d20baf20331560e8bc51eac3dfdb122
Erick Jimenez <erick.jimenez@unosquare.com>
2021-02-07 14:00:02 -0600

    Created flight simulator

--- NUnitErick/Test.cs
+++ NUnitErick/Test.cs
@@ -1,4 +1,5 @@
 ﻿using Inheritance.Exercise2;
+using Inheritance.Exercise3;
 using NUnit.Framework;
 using System;
 namespace NUnitErick
@@ -40,9 +41,35 @@ namespace NUnitErick
         }
 
         [Test()]
-        public void PendingName()
+        public void MyCustomAirline()
         {
-            
+            Customer c1 = new Customer("ABC1234", "Erick", "Jiménez Rodríguez", Countries.MX, new DateTime(1990, 6, 25), Gender.Male, "Eduardo");
+            Customer c2 = new Customer("MPX6187", "Ana", "González Rangel", Countries.MX, new DateTime(1988, 3, 8), Gender.Female, "Karen");
+            Customer c3 = new Customer("QMX6179", "Dua", "Lipa", Countries.GB, new DateTime(1995, 8, 22), Gender.Female);
+            Customer c4 = new Customer("HYA1252", "Kate", "Johnson", Countries.US, new DateTime(2005, 01, 01), Gender.Female);
+            Customer c5 = new Customer("HYA1253", "Michael", "Johnson", Countries.US, new DateTime(2005, 01, 01), Gender.Male);
+
+            c1.PrintPersonDetails();
+            //c2.PrintPersonDetails();
+            //c3.PrintPersonDetails();
+            //c4.PrintPersonDetails();
+            //c5.PrintPersonDetails();
+            Console.WriteLine("Full name por 5th person: " + c5.GetFullName());
+
+            Flight myFlight1 = new Flight(Airlines.Aeromexico, Airplanes.Jet123, 643, "Guadalajara", "Chicago");
+
+            myFlight1.PrintSeatInformation(false);
+
+            myFlight1.AssignSeat(7, c1);
+            myFlight1.AssignSeat(11, c2);
+            myFlight1.AssignSeat(12, c3);
+            myFlight1.AssignSeat(2, c4);
+            myFlight1.AssignSeat(3, c5);            
+
+            myFlight1.PrintSeatInformation(true);
+            myFlight1.PrintSeatInformation(false);
+
+            myFlight1.PrintAllPassengersInfo();
         }
     }
 }
